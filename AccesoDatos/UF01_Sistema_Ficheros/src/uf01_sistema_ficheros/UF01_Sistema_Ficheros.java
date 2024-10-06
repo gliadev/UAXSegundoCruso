95224ecbb35abf3d1e3b8508af26dd6307522330
gliadev <gliatem@gmail.com>
2024-10-06 23:45:04 +0200

    empezando estructura

---
+++ AccesoDatos/UF01_Sistema_Ficheros/src/uf01_sistema_ficheros/UF01_Sistema_Ficheros.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uf01_sistema_ficheros;
+
+/**
+ *
+ * @author adolfo
+ */
+public class UF01_Sistema_Ficheros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
