30c9ae02f1730b42821cc677ff4be24d194ba375
gliadev <gliatem@gmail.com>
2024-10-13 16:47:38 +0200

    desde ubuntu

--- AccesoDatos/UF01_Sistema_Ficheros/src/uf01_sistema_ficheros/UF01_Sistema_Ficheros.java
+++ AccesoDatos/UF01_Sistema_Ficheros/src/uf01_sistema_ficheros/UF01_Sistema_Ficheros.java
@@ -15,6 +15,7 @@ public class UF01_Sistema_Ficheros {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola caracola");
     }
     
 }
